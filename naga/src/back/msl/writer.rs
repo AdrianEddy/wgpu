AdrianEddy/wgpu

commit ce96254f4f11e9a1f8069b2f6c09b6ac66cc39b7

[msl] fix vertex pulling with a stride of 0 (#8265)

cts_runner/test.lst
@@ -18,7 +18,7 @@ webgpu:api,operation,compute,basic:memcpy:*
 webgpu:api,operation,compute_pipeline,overrides:*
 webgpu:api,operation,device,lost:*
 webgpu:api,operation,render_pass,storeOp:*
-fails-if(metal,vulkan) webgpu:api,operation,vertex_state,correctness:array_stride_zero:*
+fails-if(vulkan) webgpu:api,operation,vertex_state,correctness:array_stride_zero:*
 // Presumably vertex pulling, revisit after https://github.com/gfx-rs/wgpu/issues/7981 is fixed.
 fails-if(metal) webgpu:api,operation,vertex_state,correctness:setVertexBuffer_offset_and_attribute_offset:*
 fails-if(dx12) webgpu:api,validation,capability_checks,limits,maxBindGroups:setBindGroup,*

naga/src/back/msl/writer.rs
@@ -6359,7 +6359,7 @@ template <typename A>
         struct VertexBufferMappingResolved<'a> {
             id: u32,
             stride: u32,
-            indexed_by_vertex: bool,
+            step_mode: back::msl::VertexBufferStepMode,
             ty_name: String,
             param_name: String,
             elem_name: String,
@@ -6395,10 +6395,14 @@ template <typename A>
                     "Vertex pulling requires a non-zero buffer stride."
                 );
 
-                if vbm.indexed_by_vertex {
-                    needs_vertex_id = true;
-                } else {
-                    needs_instance_id = true;
+                match vbm.step_mode {
+                    back::msl::VertexBufferStepMode::Constant => {}
+                    back::msl::VertexBufferStepMode::ByVertex => {
+                        needs_vertex_id = true;
+                    }
+                    back::msl::VertexBufferStepMode::ByInstance => {
+                        needs_instance_id = true;
+                    }
                 }
 
                 let buffer_ty = self.namer.call(format!("vb_{buffer_id}_type").as_str());
@@ -6408,7 +6412,7 @@ template <typename A>
                 vbm_resolved.push(VertexBufferMappingResolved {
                     id: buffer_id,
                     stride: buffer_stride,
-                    indexed_by_vertex: vbm.indexed_by_vertex,
+                    step_mode: vbm.step_mode,
                     ty_name: buffer_ty,
                     param_name: buffer_param,
                     elem_name: buffer_elem,
@@ -7199,8 +7203,6 @@ template <typename A>
             }
 
             if do_vertex_pulling {
-                assert!(needs_vertex_id || needs_instance_id);
-
                 let mut separator = if is_first_argument {
                     is_first_argument = false;
                     ' '
@@ -7278,16 +7280,22 @@ template <typename A>
 
                     let idx = &vbm.id;
                     let stride = &vbm.stride;
-                    let index_name = if vbm.indexed_by_vertex {
-                        if let Some(ref name) = v_existing_id {
-                            name
-                        } else {
-                            &v_id
+                    let index_name = match vbm.step_mode {
+                        back::msl::VertexBufferStepMode::Constant => "0",
+                        back::msl::VertexBufferStepMode::ByVertex => {
+                            if let Some(ref name) = v_existing_id {
+                                name
+                            } else {
+                                &v_id
+                            }
                         }
-                    } else if let Some(ref name) = i_existing_id {
-                        name
-                    } else {
-                        &i_id
+                        back::msl::VertexBufferStepMode::ByInstance => {
+                            if let Some(ref name) = i_existing_id {
+                                name
+                            } else {
+                                &i_id
+                            }
+                        }
                     };
                     write!(
                         self.out,

naga/src/back/msl/mod.rs
@@ -418,6 +418,17 @@ pub enum VertexFormat {
     Unorm8x4Bgra = 44,
 }
 
+/// Defines how to advance the data in vertex buffers.
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serialize", derive(serde::Serialize))]
+#[cfg_attr(feature = "deserialize", derive(serde::Deserialize))]
+pub enum VertexBufferStepMode {
+    Constant,
+    #[default]
+    ByVertex,
+    ByInstance,
+}
+
 /// A mapping of vertex buffers and their attributes to shader
 /// locations.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -446,9 +457,8 @@ pub struct VertexBufferMapping {
     pub id: u32,
     /// Size of the structure in bytes
     pub stride: u32,
-    /// True if the buffer is indexed by vertex, false if indexed
-    /// by instance.
-    pub indexed_by_vertex: bool,
+    /// Vertex buffer step mode
+    pub step_mode: VertexBufferStepMode,
     /// Vec of the attributes within the structure
     pub attributes: Vec<AttributeMapping>,
 }

naga/tests/in/wgsl/msl-vpt-formats-x1.toml
@@ -49,5 +49,5 @@ attributes = [
     { offset = 640, shader_location = 40, format = "unorm8x4-bgra" },
 ]
 id = 1
-indexed_by_vertex = true
+step_mode = "ByVertex"
 stride = 644

naga/tests/in/wgsl/msl-vpt-formats-x2.toml
@@ -49,5 +49,5 @@ attributes = [
     { offset = 640, shader_location = 40, format = "unorm8x4-bgra" },
 ]
 id = 1
-indexed_by_vertex = true
+step_mode = "ByVertex"
 stride = 644

naga/tests/in/wgsl/msl-vpt-formats-x3.toml
@@ -49,5 +49,5 @@ attributes = [
     { offset = 640, shader_location = 40, format = "unorm8x4-bgra" },
 ]
 id = 1
-indexed_by_vertex = true
+step_mode = "ByVertex"
 stride = 644

naga/tests/in/wgsl/msl-vpt-formats-x4.toml
@@ -49,5 +49,5 @@ attributes = [
     { offset = 640, shader_location = 40, format = "unorm8x4-bgra" },
 ]
 id = 1
-indexed_by_vertex = true
+step_mode = "ByVertex"
 stride = 644

naga/tests/in/wgsl/msl-vpt.toml
@@ -10,11 +10,11 @@ attributes = [
     { offset = 4, shader_location = 1, format = "Float32x4" },
 ]
 id = 1
-indexed_by_vertex = true
+step_mode = "ByVertex"
 stride = 20
 
 [[msl_pipeline.vertex_buffer_mappings]]
 attributes = [{ offset = 0, shader_location = 2, format = "Float32x2" }]
 id = 2
-indexed_by_vertex = false
+step_mode = "ByInstance"
 stride = 16

commit 1072b87894b7c3084f80bc9d2679e822435186ba

Fix stray comma in function signatures in Metal (#8311)
Fixes a regression introduced by #8265 where we were mistakenly writing a comma before the first buffer argument (if no other arguments were printed before it).

cts_runner/test.lst
@@ -157,3 +157,4 @@ webgpu:shader,validation,statement,statement_behavior:invalid_statements:body="l
 webgpu:shader,validation,statement,statement_behavior:invalid_statements:body="loop8"
 webgpu:shader,validation,statement,statement_behavior:invalid_statements:body="switch1"
 //FAIL: 9 invalid_statements subtests due to https://github.com/gfx-rs/wgpu/issues/7733
+webgpu:api,validation,render_pipeline,vertex_state:many_attributes_overlapping:*

naga/src/back/msl/writer.rs
@@ -6904,16 +6904,25 @@ template <typename A>
 
             // Write the entry point function's name, and begin its argument list.
             writeln!(self.out, "{em_str} {result_type_name} {fun_name}(")?;
+
             let mut is_first_argument = true;
+            let mut separator = || {
+                if is_first_argument {
+                    is_first_argument = false;
+                    ' '
+                } else {
+                    ','
+                }
+            };
 
             // If we have produced a struct holding the `EntryPoint`'s
             // `Function`'s arguments' varyings, pass that struct first.
             if has_varyings {
                 writeln!(
                     self.out,
-                    "  {stage_in_name} {varyings_member_name} [[stage_in]]"
+                    "{} {stage_in_name} {varyings_member_name} [[stage_in]]",
+                    separator()
                 )?;
-                is_first_argument = false;
             }
 
             let mut local_invocation_id = None;
@@ -6953,13 +6962,7 @@ template <typename A>
                 };
 
                 let resolved = options.resolve_local_binding(binding, in_mode)?;
-                let separator = if is_first_argument {
-                    is_first_argument = false;
-                    ' '
-                } else {
-                    ','
-                };
-                write!(self.out, "{separator} {ty_name} {name}")?;
+                write!(self.out, "{} {ty_name} {name}", separator())?;
                 resolved.try_fmt(&mut self.out)?;
                 writeln!(self.out)?;
             }
@@ -6968,15 +6971,9 @@ template <typename A>
                 self.need_workgroup_variables_initialization(options, ep, module, fun_info);
 
             if need_workgroup_variables_initialization && local_invocation_id.is_none() {
-                let separator = if is_first_argument {
-                    is_first_argument = false;
-                    ' '
-                } else {
-                    ','
-                };
                 writeln!(
                     self.out,
-                    "{separator} {NAMESPACE}::uint3 __local_invocation_id [[thread_position_in_threadgroup]]"
+                    "{} {NAMESPACE}::uint3 __local_invocation_id [[thread_position_in_threadgroup]]", separator()
                 )?;
             }
 
@@ -7123,15 +7120,6 @@ template <typename A>
                     }
                 }
 
-                let mut separator = || {
-                    if is_first_argument {
-                        is_first_argument = false;
-                        ' '
-                    } else {
-                        ','
-                    }
-                };
-
                 match module.types[var.ty].inner {
                     crate::TypeInner::Image {
                         class: crate::ImageClass::External,
@@ -7203,21 +7191,13 @@ template <typename A>
             }
 
             if do_vertex_pulling {
-                let mut separator = if is_first_argument {
-                    is_first_argument = false;
-                    ' '
-                } else {
-                    ','
-                };
-
                 if needs_vertex_id && v_existing_id.is_none() {
                     // Write the [[vertex_id]] argument.
-                    writeln!(self.out, "{separator} uint {v_id} [[vertex_id]]")?;
-                    separator = ',';
+                    writeln!(self.out, "{} uint {v_id} [[vertex_id]]", separator())?;
                 }
 
                 if needs_instance_id && i_existing_id.is_none() {
-                    writeln!(self.out, "{separator} uint {i_id} [[instance_id]]")?;
+                    writeln!(self.out, "{} uint {i_id} [[instance_id]]", separator())?;
                 }
 
                 // Iterate vbm_resolved, output one argument for every vertex buffer,
@@ -7228,7 +7208,8 @@ template <typename A>
                     let param_name = &vbm.param_name;
                     writeln!(
                         self.out,
-                        ", const device {ty_name}* {param_name} [[buffer({id})]]"
+                        "{} const device {ty_name}* {param_name} [[buffer({id})]]",
+                        separator()
                     )?;
                 }
             }
@@ -7238,10 +7219,10 @@ template <typename A>
             if needs_buffer_sizes {
                 // this is checked earlier
                 let resolved = options.resolve_sizes_buffer(ep).unwrap();
-                let separator = if is_first_argument { ' ' } else { ',' };
                 write!(
                     self.out,
-                    "{separator} constant _mslBufferSizes& _buffer_sizes",
+                    "{} constant _mslBufferSizes& _buffer_sizes",
+                    separator()
                 )?;
                 resolved.try_fmt(&mut self.out)?;
                 writeln!(self.out)?;